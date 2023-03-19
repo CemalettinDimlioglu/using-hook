8ed2ca2406d9b20ee97bc09fa180e4baab980178
CMLTTN <cemaldim@gmail.com>
2023-03-19 18:39:03 +0300

    UseRef

--- rc20_memo-useMemo-useCallback-useRef/hooks-inclass/src/components/UseRefComp.jsx
+++ rc20_memo-useMemo-useCallback-useRef/hooks-inclass/src/components/UseRefComp.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const UseRefComp = () => {
+  console.log("Render =>useRefComp");
+  const [text, setText]= useState("")
+  const inputRef = useRef()
+  const handleSearch = ()=>{
+    console.log(text);
+    console.log(inputRef);
+  }
   return (
     <div>
       UseRefComp
       <div style={{ display: "flex", justifyContent: "center" }}>
-        <input type="text" />
-        <button type="button">
+        <input type="text" ref={inputRef} onChange={(e)=>setText(e.target.value)} />
+        <button type="button" onClick={handleSearch()} >
           Search
         </button>
       </div>
